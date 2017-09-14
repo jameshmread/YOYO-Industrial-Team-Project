a936d05bdc7248aa57f72a8d4871add4c68e9ca8
link5478 <cheyne.carsten@gmail.com>
2017-09-14 12:52:28 +0100

    Added a logout button to the Welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,6 +70,7 @@
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ route('logout') }}">Log-Out</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
