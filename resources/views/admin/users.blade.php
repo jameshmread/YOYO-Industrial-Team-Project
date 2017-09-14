719214a2c05934bbaf1edd314112e09380e0125c
Anthony Grimes <contact@anthonygrimes.co.uk>
2017-09-14 17:47:11 +0100

    admin users styling

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -11,54 +11,54 @@
                 @if (Session::has('success'))
                     <p class="alert alert-success">{{ Session::get('success') }}</p>
                 @endif
+            </div>
+        </div>
 
-                <div class="row">
-                    <div class="col-md-3">
-                        <form action="{{route('admin.users.create')}}" method="GET">
-                            <button type="submit" class="btn btn-primary">Add User</button>
-                        </form>
-                    </div>
-                </div>
+        <div class="row">
+            <div class="col-md-3">
+                <form action="{{route('admin.users.create')}}" method="GET">
+                    <button type="submit" class="btn btn-primary">Add User</button>
+                </form>
+            </div>
+        </div>
 
-                <br>
+        <br>
 
-                <div class="row">
-                    <div class="col-md-12">
-                        <table class="table">
-                            <tr>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th></th>
-                                <th></th>
-                            </tr>
+        <div class="row">
+            <div class="col-md-12">
+                <table class="table">
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
 
-                            @foreach($users as $user)
-                                <tr>
+                    @foreach($users as $user)
+                        <tr>
 
-                                    <td width="20%">
-                                        {{$user->name}}
-                                    </td>
+                            <td width="20%">
+                                {{$user->name}}
+                            </td>
 
-                                    <td width="25%"><a href="mailto:{{$user->email}}">{{$user->email}}</a></td>
+                            <td width="25%"><a href="mailto:{{$user->email}}">{{$user->email}}</a></td>
 
-                                    <td width="20%">
-                                        <a class="btn btn-primary" href="{{ route('admin.users.edit', $user->id) }}">Edit</a>
-                                    </td>
+                            <td width="20%">
+                                <a class="btn btn-primary" href="{{ route('admin.users.edit', $user->id) }}">Edit</a>
+                            </td>
 
-                                    <td width="20%">
-                                        <form action="{{ route('admin.users.destroy', $user->id) }}" method="POST">
-                                            {{ csrf_field() }}
-                                            {{ method_field('DELETE') }}
+                            <td width="20%">
+                                <form action="{{ route('admin.users.destroy', $user->id) }}" method="POST">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
 
-                                            <button type="submit" class="btn btn-primary">Delete</button>
-                                        </form>
-                                    </td>
+                                    <button type="submit" class="btn btn-primary">Delete</button>
+                                </form>
+                            </td>
 
-                                </tr>
-                            @endforeach
-                        </table>
-                    </div>
-                </div>
+                        </tr>
+                    @endforeach
+                </table>
             </div>
         </div>
     </div>
