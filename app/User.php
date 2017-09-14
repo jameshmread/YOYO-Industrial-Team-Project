ac38175c3441737fcb1116debda731c5e358af98
link5478 <cheyne.carsten@gmail.com>
2017-09-14 01:16:03 +0100

    Add function to check if the user is an admin

--- app/User.php
+++ app/User.php
@@ -29,4 +29,13 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+
+    public function isAdmin(){
+
+        return $this->admin ? true : false;
+
+
+
+    }
 }
