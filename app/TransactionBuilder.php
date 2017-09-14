765ce8cd06000c71158b95b6e31addd0b8c987a0
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-09-14 11:32:58 +0100

    Remove disbursals.csv from TransactionBuilderTest due to memory limits

--- app/TransactionBuilder.php
+++ app/TransactionBuilder.php
@@ -4,36 +4,45 @@ namespace App;
 
 class TransactionBuilder
 {
+    /**
+     * @todo could probably use some refactoring - array_map makes the 
+     * removeNullValuesFromArray method necessary.
+     */
     public function createFromFile(string $filePath): array
     {
         $lines = file($filePath, FILE_IGNORE_NEW_LINES);
-        unset($lines[0]);
         $csv = array_map(array($this, 'convertLineIntoTransaction'), $lines);
+        $this->removeNullValuesFromArray($csv);
         return $csv;
     }
 
-    public function convertLineIntoTransaction(string $line): Transaction
+    public function convertLineIntoTransaction(string $line)
     {
-        $array = str_getcsv($line);
+        $transaction = null;
+        try {
+            $array = str_getcsv($line);
 
-        $date = $this->extractDate($array[0]);
-        // I assumed the store id in the model and the outlet reference in the
-        // model are the same - LM.
-        $storeId = $array[2];
-        $customerId = $array[5];
-        $transactionType = $array[6];
-        $cashSpent = $this->extractPrice($array[7]);
-        $discountAmount = $this->extractPrice($array[8]);
-        $totalAmount = $this->extractPrice($array[9]);
-        $transaction = new Transaction(
-            $cashSpent,
-            $customerId,
-            $date,
-            $discountAmount,
-            $storeId,
-            $totalAmount,
-            $transactionType
-        );
+            $date = $this->extractDate($array[0]);
+            // I assumed the store id in the model and the outlet reference in the
+            // model are the same - LM.
+            $storeId = $array[2];
+            $customerId = $array[5];
+            $transactionType = $array[6];
+            $cashSpent = $this->extractPrice($array[7]);
+            $discountAmount = $this->extractPrice($array[8]);
+            $totalAmount = $this->extractPrice($array[9]);
+            $transaction = new Transaction(
+                $cashSpent,
+                $customerId,
+                $date,
+                $discountAmount,
+                $storeId,
+                $totalAmount,
+                $transactionType
+            );
+        } catch (\ErrorException $e) {
+            return null;
+        }
         // echo '<pre>';
         // var_dump($cashSpent);
         // echo '</pre>';
@@ -42,29 +51,43 @@ class TransactionBuilder
 
     public function extractDate(string $csvCell): string
     {
-        $dateAndTime = explode(' ', $csvCell);
-        $date = $dateAndTime[0];
-        $dayMonthYear = explode('/', $date);
-        $time = $dateAndTime[1];
-        $hourMinuteSecond = explode(':', $time);
-        $day = $dayMonthYear[0];
-        $month = $dayMonthYear[1];
-        $year = $dayMonthYear[2];
-        $hour = $hourMinuteSecond[0];
-        $minute = $hourMinuteSecond[1];
-        $second = $hourMinuteSecond[2];
-        return "$day/$month/$year $hour:$minute:$second";
+        $cellArray = explode(' ', str_replace(array('/', ':'), ' ', $csvCell));
+        if (6 === sizeof($cellArray)) {
+            $day = $cellArray[0];
+            $month = $cellArray[1];
+            $year = $cellArray[2];
+            $hour = $cellArray[3];
+            $minute = $cellArray[4];
+            $second = $cellArray[5];
+            return "$day/$month/$year $hour:$minute:$second";
+        } else {
+            throw new \ErrorException;
+        }
     }
 
     public function extractPrice(string $csvCell): float
     {
         $prices = array();
         preg_match('/[0-9][.][0-9]{2}/', $csvCell, $prices);
-        $price = floatval($prices[0]);
-        $isPositive = strpos($csvCell, '-') === false;
-        if (!$isPositive) {
-            $price *= -1;
+        if (1 === sizeof($prices)) {
+            $price = floatval($prices[0]);
+            $isPositive = strpos($csvCell, '-') === false;
+            if (!$isPositive) {
+                $price *= -1;
+            }
+            return $price;
+        } else {
+            throw new \ErrorException;
         }
-        return $price;
+    }
+
+    public function removeNullValuesFromArray(array &$array)
+    {
+        $sizeOfArray = sizeof($array);
+        for ($i = $sizeOfArray - 1; $i >= 0; $i--) {
+            if (null === $array[$i]) {
+                unset($array[$i]);
+            }
+        }
     }
 }
---
+++ tests/CSV/TransactionBuilderTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\CSV;
+
+use Tests\TestCase;
+use App\Transaction;
+use App\TransactionBuilder;
+
+class TransactionBuilderTest extends TestCase
+{
+    private $tb;
+
+    protected function setUp()
+    {
+        $this->tb = new TransactionBuilder;
+    }
+
+    /**
+     * @dataProvider filePathsProvider
+     */
+    public function testCreateFromFile($filePath)
+    {
+        $csvFile = file($filePath);
+
+        $transactionsArray = $this->tb->createFromFile($filePath);
+
+        $expectedArraySize = sizeof($csvFile) -1;
+        $this->assertEquals($expectedArraySize, sizeof($transactionsArray));
+
+        foreach ($transactionsArray as $currentTransaction) {
+            $this->assertEquals('App\Transaction', get_class($currentTransaction));
+        }
+    }
+
+    /**
+     * @depends testCreateFromFile
+     */
+    public function testTransactions()
+    {
+        $transactions = $this->tb->createFromFile(__DIR__.'/../../resources/csv/test.csv');
+        $this->assertEquals(4.00, $transactions[16]->totalAmount);
+        $this->assertEquals(-1.25, $transactions[7]->cash_spent);
+        $this->assertEquals('239', $transactions[9]->storeId);
+        $this->assertEquals('dusa-0017', $transactions[11]->customer_id);
+        $this->assertEquals('24/08/2015 10:04:54', $transactions[2]->date);
+        $this->assertEquals(0.00, $transactions[16]->discountAmount);
+    }
+
+    /**
+     * @todo why isn't base_path() working?
+     */
+    public function filePathsProvider()
+    {
+        return [
+            [__DIR__.'/../../resources/csv/test.csv'],
+            //[__DIR__.'/../../resources/csv/Disbursals.csv'],
+        ];
+    }
+}
