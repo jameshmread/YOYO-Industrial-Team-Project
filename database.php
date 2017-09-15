8d00681c600eceffff0213d837960aef092e6fdd
jameshmread <jhmread@dundee.ac.uk>
2017-09-15 12:57:49 +0100

    variables modified

--- database.php
+++ database.php
@@ -6,26 +6,15 @@ return [
     'connections' => [
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'homestead'),
-            'username'  => env('DB_USERNAME', 'homestead'),
-            'password'  => env('DB_PASSWORD', 'secret'),
+            'host'      => getenv('DB_HOST', 'localhost'),
+            'database'  => getenv('DB_DATABASE', 'homestead'),
+            'username'  => getenv('DB_USERNAME', 'homestead'),
+            'password'  => getenv('DB_PASSWORD', 'secret'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
             'strict'    => false,
-        ],
-        'testing' => [
-            'driver'    => 'mysql',
-            'host'      => env('DB_TEST_HOST', 'localhost'),
-            'database'  => env('DB_TEST_DATABASE', 'homestead_test'),
-            'username'  => env('DB_TEST_USERNAME', 'homestead'),
-            'password'  => env('DB_TEST_PASSWORD', 'secret'),
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-            'strict'    => false,
-        ],
+        ]
     ],
     //...
 ];
