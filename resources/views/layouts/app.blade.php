f727409ab2b90dd623ad2ba282cb9997ca1d07f3
Anthony Grimes <contact@anthonygrimes.co.uk>
2017-09-16 20:03:45 +0100

    Add admin link on nav bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,6 +45,13 @@
                         @guest
                             <li><a href="{{ route('login') }}">Login</a></li>
                         @else
+
+                            @if(Auth::user()->is_admin === 1)
+                                <li>
+                                    <a href="{{route('admin.dashboard')}}">Admin Dashboard</a>
+                                </li>
+                            @endif
+
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
