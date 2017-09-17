b54b83af5982b4f8216751e84d884a1d1c5d9f54
tomcatooo <tomasdrobinson@gmail.com>
2017-09-17 21:13:21 +0100

    added edit user page

---
+++ resources/views/edituser.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<h1> {{ $user->name }}</h1>
+<h2> {{ $user->email }}</h2>
+
+<p> Edit details </p>
+
+<form action="/users" method="POST">
+
+{{ csrf_field() }}
+  Name:<br>
+  <input type="text" id="name" name="name" 
+  placeholder="{{$user->name}}"
+  value="{{ Request::old('name', $user->name) }}"><br>
+
+  <input type="text" id="email" name="email" 
+  placeholder="{{$user->email}}"
+  value="{{ Request::old('email', $user->email) }}"><br>
+  
+  <input type="hidden" id="id" name="id" 
+  placeholder="{{$user->id}}"
+  value="{{ Request::old('id', $user->id) }}">
+  <input type="submit" value="Submit" >
+</form>
+
+<br>
+<a href="/home"> Home </a>
+
+</div>
