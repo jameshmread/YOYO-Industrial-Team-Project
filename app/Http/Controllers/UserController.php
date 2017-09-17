ee8109d720aab52ae8bfda07ca87a786c8df12f7
Anthony Grimes <contact@anthonygrimes.co.uk>
2017-09-17 23:32:16 +0100

    Dirty auth checking

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\User;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function edit($id)
     {
+        // TODO There is a better way to do this, but I forget late at night
+        if (Auth::user()->id != $id) {
+            return redirect('home');
+        }
+
         $user = User::find($id);
 
         return view('edituser', compact('user'));
@@ -17,6 +22,11 @@ class UserController extends Controller
 
     public function update(Request $request, User $user)
     {
+        // TODO There is a better way to do this, but I forget late at night
+        if (Auth::user()->id != $user->id) {
+            return redirect('home');
+        }
+
         $request->validate([
             'name' => 'alpha',
             'email' => 'email',
