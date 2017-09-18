7c72f806fd12a21cadd98d6768c6bb19501af071
link5478 <cheyne.carsten@gmail.com>
2017-09-18 01:21:10 +0100

    Create vue component for pie chart

---
+++ resources/assets/js/components/transchartPie.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="Chart">
+            <h1 style="text-align:center;">Some data</h1>
+            <pie-chart></pie-chart>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    import PieChart from './piechart.js'
+    export default
+    {
+        components:
+            {
+                'pie-chart': PieChart
+            }
+    }
+</script>
+
+<style>
+    .container {
+        max-width: 800px;
+        margin: 0 auto;
+    }
+    h1 {
+        font-family: 'Helvetica', Arial;
+        color: #464646;
+        text-transform: uppercase;
+        border-bottom: 1px solid #f1f1f1;
+        padding-bottom: 15px;
+        font-size: 28px;
+        margin-top: 0;
+    }
+    .Chart {
+        padding: 20px;
+        box-shadow: 0px 0px 20px 2px rgba(0, 0, 0, .4);
+        border-radius: 20px;
+        margin: 50px 0;
+    }
+</style>
