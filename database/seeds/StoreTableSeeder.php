5b6239422db0d72a9c927ea4e35593a0c0c0b3ee
Broxibear97 <hogarthjoshie@aol.com>
2017-09-18 02:37:04 +0100

    Removed random stores.
    It hit me that we dont need random stores. we have a limited amount.
    I can just generate those.

--- database/seeds/StoreTableSeeder.php
+++ database/seeds/StoreTableSeeder.php
@@ -11,6 +11,45 @@ class StoreTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Store::class, 30)->create();
+        DB::table('stores')->insert(
+            [
+                'outlet_reference' => 238,
+                'outlet_name' => 'Library'
+            ]);
+        DB::table('stores')->insert(
+            [
+                'outlet_reference' => 239,
+                'outlet_name' => 'Spare'
+            ]);
+        DB::table('stores')->insert(
+            [
+            'outlet_reference' => 236,
+                'outlet_name' => 'Air Bar'
+            ]);
+        DB::table('stores')->insert(
+            [
+                'outlet_reference' => 243,
+                'outlet_name' => 'Ents'
+            ]);
+        DB::table('stores')->insert(
+            [
+                'outlet_reference' => 343,
+                'outlet_name' => 'Remote Campus Shop'
+            ]);
+        DB::table('stores')->insert(
+            [
+                'outlet_reference' => 241,
+                'outlet_name' => 'Liar Bar'
+            ]);
+        DB::table('stores')->insert(
+            [
+                'outlet_reference' => 242,
+                'outlet_name' => 'Mono'
+            ]);
+        DB::table('stores')->insert(
+            [
+                'outlet_reference' => 240,
+                'outlet_name' => 'Food On Four'
+            ]);
     }
 }
