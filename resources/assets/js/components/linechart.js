494e2c4b414886de2e5ef8cf892be3a963a5c1fd
link5478 <cheyne.carsten@gmail.com>
2017-09-18 01:20:16 +0100

    Code to generate a line chart (For Demo)

---
+++ resources/assets/js/components/linechart.js
@@ -0,0 +1,50 @@
+import {Line} from 'vue-chartjs'
+
+export default Line.extend(
+    {
+        mounted ()
+        {
+            axios.get('/api/transactions/recent')
+                .then(response=> {
+                console.log(response.data);
+
+            this.labels = response.data.map(x => { return x.date});
+
+            this.rows = response.data.map(x => {return x.total_amount});
+
+            this.setGraph();
+        });
+        },
+
+        data()
+        {
+            return {
+                rows: [],
+                labels: []
+            }
+        },
+
+
+
+        methods:
+            {
+                onlyUnique(value, index, self) {
+                    return self.indexOf(value) === index;
+                },
+
+                setGraph()
+                {
+                    this.renderChart({
+                        labels: this.labels,
+                        datasets: [
+                            {
+                                label: 'Transaction',
+                                backgroundColor: 'blue',
+                                data: this.rows,
+                            }
+                        ]
+                    }, {responsive: true, maintainAspectRatio: false})
+                }
+            }
+    }
+)
