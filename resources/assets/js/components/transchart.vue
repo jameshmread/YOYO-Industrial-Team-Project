d88db320fbf6343023309b94e749aa7af70f4736
link5478 <cheyne.carsten@gmail.com>
2017-09-19 12:36:42 +0100

    Added a drop down menu that displays all stores. (For future work)

--- resources/assets/js/components/transchart.vue
+++ resources/assets/js/components/transchart.vue
@@ -1,6 +1,31 @@
 <template>
     <div class="container">
         <div class="Chart">
+
+            <select id="StoreDropDown" onChange="store_id()">
+                <option value="0">All</option>
+                <option value="1">Library</option>
+                <option value="2">Spare</option>
+                <option value="3">Air Bar</option>
+                <option value="4">Ents</option>
+                <option value="5">Remote Campus Shop</option>
+                <option value="6">Liar Bar</option>
+                <option value="7">Mono</option>
+                <option value="8">Food on Four</option>
+                <option value="9">College Shop</option>
+                <option value="10">Dental Cafe</option>
+                <option value="11">DJCAD Cantina</option>
+                <option value="12">DOJ Catering</option>
+                <option value="13">DUSA The Union: Marketplace</option>
+                <option value="14">DUSA The Union: Online</option>
+                <option value="15">Floor Five</option>
+                <option value="16">Level 2, Reception</option>
+                <option value="17">Ninewells Shop</option>
+                <option value="18">Online DUSA</option>
+                <option value="19">Premier Shop</option>
+                <option value="20">Premier Shop YoYo</option>
+            </select>
+
             <h1 style="text-align:center;">Some data</h1>
             <line-chart></line-chart>
 
@@ -10,6 +35,12 @@
 
 <script>
     import LineChart from './linechart.js'
+
+    function store_id() {
+        var e = document.getElementById("StoreDropDown");
+        var value = e.options[e.selectedIndex].value;
+    }
+
     export default
     {
         components:
