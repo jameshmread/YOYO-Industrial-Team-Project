acca34fcb0c0bb35e849b3909fa331a06dbc36a6
link5478 <cheyne.carsten@gmail.com>
2017-09-19 11:09:28 +0100

    Added a basic graph js file for testing

---
+++ resources/assets/js/components/linechart.js
@@ -0,0 +1,50 @@
+import {Line} from 'vue-chartjs'
+
+export default Line.extend(
+    {
+        mounted ()
+        {
+            axios.get('/api/transactions/totalbystore')
+                .then(response=> {
+                    console.log(response.data);
+
+                    this.labels = response.data.map(x => { return x.store_id});
+
+                    this.rows = response.data.map(x => {return x.total});
+
+                    this.setGraph();
+                });
+        },
+
+        data()
+        {
+            return {
+                rows: [],
+                labels: []
+            }
+        },
+
+
+
+        methods:
+            {
+                onlyUnique(value, index, self) {
+                    return self.indexOf(value) === index;
+                },
+
+                setGraph()
+                {
+                    this.renderChart({
+                        labels: this.labels,
+                        datasets: [
+                            {
+                                label: 'Transaction',
+                                backgroundColor: 'blue',
+                                data: this.rows,
+                            }
+                        ]
+                    }, {responsive: true, maintainAspectRatio: false})
+                }
+            }
+    }
+)
