0cf18ff40d4be7c570faf4d8d8a620525326ab36
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-09-20 15:02:54 +0100

    Move API routes from web.php to api.php

--- routes/web.php
+++ routes/web.php
@@ -61,18 +61,3 @@ $router->group([
     $router->get('/csvupload', 'CSVController@index')->name('upload.index');
     $router->post('/csvupload', 'CSVController@upload')->name('upload');
 });
-
-/**
- * API ROUTES
- */
-$router->group([
-    'middleware' => ['auth'],
-    'prefix' => 'api',
-    'as' => 'api.'
-], function (Router $router) {
-    $router->get('/transactions/period/{period1}/{period2}', 'APIController@periodToPeriod')->name('periodToPeriod');
-    $router->get('/transactions/recent', 'APIController@recentTransactions')->name('recentTransactions');
-    $router->get('/transactions/{year}', 'APIController@dmyListing')->name('yearlyListing');
-    $router->get('/transactions/{year}/{month}', 'APIController@dmyListing')->name('monthlyListing');
-    $router->get('/transactions/{year}/{month}/{day}', 'APIController@dmyListing')->name('dailyListing');
-});
