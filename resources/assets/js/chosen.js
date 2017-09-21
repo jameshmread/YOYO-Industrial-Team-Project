2c1b70e25ea60877945a0421fc75e451ec75dcf9
Broxibear97 <hogarthjoshie@aol.com>
2017-09-21 22:59:38 +0100

    Fixed merge conflict.

--- resources/assets/js/chosen.js
+++ resources/assets/js/chosen.js
@@ -2,10 +2,8 @@ $(".urights").chosen({
     no_results_text: "Oops, nothing found!"
 });
 
-<<<<<<< HEAD
 $(".storeDropDown").chosen({
     no_results_text: "Oops, nothing found!"
 });
-=======
+
 $(".reportfrequency").chosen();
->>>>>>> master
