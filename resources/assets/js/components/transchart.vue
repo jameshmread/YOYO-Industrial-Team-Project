7710a9aa6e84118f2e8ba555f55fba9f7fb71d65
Broxibear97 <hogarthjoshie@aol.com>
2017-09-21 14:21:36 +0100

    Added dropdown checkbox for comparing stores.

--- resources/assets/js/components/transchart.vue
+++ resources/assets/js/components/transchart.vue
@@ -1,30 +1,25 @@
 <template>
     <div class="container">
+        <br/>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="button-group">
+                        <button type="button" class="btn btn-default btn-sm dropdown-toggle"
+                                data-toggle="dropdown">Stores<span class="caret"></span></button>
+                        <ul class="dropdown-menu">
+                            <li><a href="#" class="small" data-value="1" tabIndex="-1"><input type="checkbox"/>&nbsp;Library</a></li>
+                            <li><a href="#" class="small" data-value="2" tabIndex="-1"><input type="checkbox"/>&nbsp;Option 2</a></li>
+                            <li><a href="#" class="small" data-value="3" tabIndex="-1"><input type="checkbox"/>&nbsp;Option 3</a></li>
+                            <li><a href="#" class="small" data-value="4" tabIndex="-1"><input type="checkbox"/>&nbsp;Option 4</a></li>
+                            <li><a href="#" class="small" data-value="5" tabIndex="-1"><input type="checkbox"/>&nbsp;Option 5</a></li>
+                            <li><a href="#" class="small" data-value="option6" tabIndex="-1"><input type="checkbox"/>&nbsp;Option 6</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="Chart">
-            <select id="StoreDropDown" onChange="store_id()">
-                <option value="0">All</option>
-                <option value="1">Library</option>
-                <option value="2">Spare</option>
-                <option value="3">Air Bar</option>
-                <option value="4">Ents</option>
-                <option value="5">Remote Campus Shop</option>
-                <option value="6">Liar Bar</option>
-                <option value="7">Mono</option>
-                <option value="8">Food on Four</option>
-                <option value="9">College Shop</option>
-                <option value="10">Dental Cafe</option>
-                <option value="11">DJCAD Cantina</option>
-                <option value="12">DOJ Catering</option>
-                <option value="13">DUSA The Union: Marketplace</option>
-                <option value="14">DUSA The Union: Online</option>
-                <option value="15">Floor Five</option>
-                <option value="16">Level 2, Reception</option>
-                <option value="17">Ninewells Shop</option>
-                <option value="18">Online DUSA</option>
-                <option value="19">Premier Shop</option>
-                <option value="20">Premier Shop YoYo</option>
-            </select>
-
             <h1 style="text-align:center;">Some data</h1>
             <Chart :chart-data="datacollection" :options="options"></Chart>
             <button @click="getData()">Get</button>
@@ -34,6 +29,31 @@
 </template>
 
 <script>
+
+    var options = [];
+
+    $( '.dropdown-menu a' ).on( 'click', function( event ) {
+
+        var $target = $( event.currentTarget ),
+            val = $target.attr( 'data-value' ),
+            $inp = $target.find( 'input' ),
+            idx;
+
+        if ( ( idx = options.indexOf( val ) ) > -1 ) {
+            options.splice( idx, 1 );
+            setTimeout( function() { $inp.prop( 'checked', false ) }, 0);
+        } else {
+            options.push( val );
+            setTimeout( function() { $inp.prop( 'checked', true ) }, 0);
+        }
+
+        $( event.target ).blur();
+
+        console.log( options );
+        return false;
+    });
+
+
     import Chart from './linechart'
 
     export default {
@@ -46,6 +66,13 @@
                 colours : ['rgba(255,0,0,0.25)',
                     'rgba(0,255,0, 0.25)',
                     'rgba(0,0,255, 0.25)',
+                    'rgba(0,0,255, 0.25)',
+                    'rgba(125,125,0, 0.25)',
+                    'rgba(125,0,125, 0.25)',
+                    'rgba(0,125,125, 0.25)',
+                    'rgba(125,75,255, 0.25)',
+                    'rgba(75,125,255, 0.25)',
+                    'rgba(255,125,75, 0.25)'
                     ],
 
                 datacollection: {
