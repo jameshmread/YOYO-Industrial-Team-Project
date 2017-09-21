e7b0ff2236f25a3c97cc7bed9d724305bae0da62
Anthony Grimes <contact@anthonygrimes.co.uk>
2017-09-21 19:37:15 +0100

    Fix broken routes middleware, include middleware for reporting category only users

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,8 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
-        \App\Http\Middleware\CheckAdmin::class
+        \App\Http\Middleware\CheckAdmin::class,
+        \App\Http\Middleware\CheckReporting::class
     ];
 
     /**
@@ -59,5 +60,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'admin' => \App\Http\Middleware\CheckAdmin::class,
+        'report' => \App\Http\Middleware\CheckReporting::class
     ];
 }
