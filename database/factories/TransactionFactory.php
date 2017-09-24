c73e9c3207fb248eb00c7b348e09504dba4cdd1c
link5478 <cheyne.carsten@gmail.com>
2017-09-24 14:16:32 +0100

    Changed factory to get correct information

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -7,11 +7,13 @@ $factory->define(App\Transaction::class, function (Faker $faker) {
     $cash = $faker->randomFloat(2, 0, 999);
     $discount = 0-$faker->randomFloat(2, 0, 999);
     $customer = App\Customer::pluck('id')->All();
-    $store = App\Store::pluck('id')->All();
+    $store = App\Store::pluck('outlet_reference')->All();
+    $store2 = App\Store::pluck('outlet_name')->All();
 
     return [
         'customer_id' => $faker->randomElement($customer),
         'store_id' => $faker->randomElement($store),
+        'outlet_name' => $faker->randomElement($store2),
         'date' => $faker->dateTimeThisMonth(),
         'transaction_type' => $faker->randomElement(['Payment', 'Refund', 'Redemption']),
         'cash_spent' => $cash,
