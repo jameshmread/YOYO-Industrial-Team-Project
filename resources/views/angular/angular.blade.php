31b353a3f15225b1958e1770fae72ce56ed072b5
jameshmread <jamesread117@yahoo.co.uk>
2017-09-24 13:52:11 +0100

    removed script tags as not needed now

--- resources/views/angular/angular.blade.php
+++ resources/views/angular/angular.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.angular')
 @section('content')
 <div class="container">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M"
-          crossorigin="anonymous">
     <title>YOYOAngular</title>
     <base href="/">
 
@@ -11,11 +9,5 @@
     <app-root>
         <script src="{{ asset('js/Chart.bundle.js') }}"></script>
     </app-root>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-            crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
-            crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1"
-            crossorigin="anonymous"></script>
 </div>
 @endsection()
