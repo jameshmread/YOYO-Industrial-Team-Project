c37b223e2d9c3a2c01b2ffa245833c9ca30ac100
jameshmread <jamesread117@yahoo.co.uk>
2017-09-24 14:07:08 +0100

    added scripts which angular cli should need

--- resources/views/layouts/angular.blade.php
+++ resources/views/layouts/angular.blade.php
@@ -122,5 +122,10 @@
     <!-- Scripts -->
     <script src="{{ asset('js/Chart.bundle.js') }}"></script>
     <script src="{{ asset('js/main.bundle.js') }}"></script>
+    <script type="text/javascript" src="inline.bundle.js"></script>
+    <script type="text/javascript" src="polyfills.bundle.js"></script>
+    <script type="text/javascript" src="styles.bundle.js"></script>
+    <script type="text/javascript" src="vendor.bundle.js"></script>
+    <script type="text/javascript" src="main.bundle.js"></script>
 </body>
 </html>
