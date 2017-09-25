e5437792cd31d7a0bb9b1e25e7e1bc66f6dda20f
link5478 <cheyne.carsten@gmail.com>
2017-09-26 00:39:46 +0100

    Vue component for a colour picker
    Took so long :O

---
+++ resources/assets/js/components/ColourPicker.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+        <chrome v-model="colors" @input="onChange"></chrome>
+    </div>
+</template>
+
+<script>
+    import {Chrome} from 'vue-color'
+
+    export default {
+        components: {
+            Chrome
+        },
+        data () {
+            return {
+                colors: {
+                    hex: '#194d33',
+                }
+            }
+        },
+        methods: {
+            // onChange method called when the event 'change-color' is emitted
+            onChange (val) {
+                this.colors = val;
+                console.log(this.colors.hex);
+            }
+        }
+    }
+</script>
