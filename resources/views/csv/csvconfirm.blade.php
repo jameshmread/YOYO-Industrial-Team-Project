a986135579593cd853d4eb3f5aa299cb8f0bb943
tomcatooo <tomasdrobinson@gmail.com>
2017-09-25 16:31:56 +0100

    Added back button after CSV upload

--- resources/views/csv/csvconfirm.blade.php
+++ resources/views/csv/csvconfirm.blade.php
@@ -5,6 +5,19 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <h1>CSV Uploaded Successfully!</h1>
+                
+                
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                @if(Auth::user()->is_admin === 1)
+                <form action="{{route('admin.dashboard')}}" method="GET">
+                    <button type="submit" class="btn btn-primary">Back</button>
+                </form>
+                @endif
+                
+                
             </div>
         </div>
     </div>
