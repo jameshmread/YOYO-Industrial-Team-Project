1cdc2e08b6ffacf84451f8f6ecbb230589acd80e
link5478 <cheyne.carsten@gmail.com>
2017-09-26 11:10:34 +0100

    Added vue component to allow an admin to change a store's colour

--- resources/assets/js/components/ColourPicker.vue
+++ resources/assets/js/components/ColourPicker.vue
@@ -1,29 +1,113 @@
 <template>
-    <div id="app">
-        <chrome v-model="colors" @input="onChange"></chrome>
+    <div class="container">
+        <div class="Chart">
+            <h1 style="text-align:center;">Change Store Colour</h1>
+
+            <div class="row">
+                <div class="col col-md-3 col-lg-3 col-sm-3" style = "width: 30%;"><multiselect
+                        v-model="selected"
+                        :options="choiceData"
+                        :searchable="false"
+                        :close-on-select="true"
+                        :show-labels="false"
+                        :hide-selected="true"
+                        placeholder="Pick a Store">
+                </multiselect></div>
+
+                <br>
+
+                <div class="col col-md-3 col-lg-3 col-sm-3">
+                    <div id="app">
+                        <chrome v-model="colors" @input="onChange"></chrome>
+                    </div>
+                </div>
+
+                <div class="col col-md-3 col-lg-3 col-sm-3">
+                    <button v-on:click="updateColour()">Update</button>
+                </div>
+            </div>
+            <br>
+
+        </div>
     </div>
 </template>
 
 <script>
     import {Chrome} from 'vue-color'
-
+    import Multiselect from 'vue-multiselect';
     export default {
         components: {
-            Chrome
+            Chrome,
+            Multiselect
         },
         data () {
             return {
                 colors: {
                     hex: '#194d33',
-                }
+                },
+
+                selected: [],
+                choiceData: [],
             }
         },
+
+        mounted () {
+            this.getStores()
+        },
+
         methods: {
             // onChange method called when the event 'change-color' is emitted
             onChange (val) {
                 this.colors = val;
+                //console.log(this.colors.hex);
+            },
+
+            getStores(){
+
+                axios.get('/api/stores').then(response =>{
+                    //console.log(response);
+
+                    this.choiceData = response.data.map(x => {return x.outlet_name});
+
+                })
+            },
+
+            updateColour(){
+
+                console.log(this.selected);
                 console.log(this.colors.hex);
+
+                var colour = this.colors.hex.slice(1, this.colors.hex.length);
+
+                axios.get('/api/colours/'+ this.selected + '/update-colours/' + colour);
+
             }
         }
     }
 </script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+
+<style>
+    .container {
+        max-width: 10000800px;
+        margin: 0 auto;
+    }
+
+    h1 {
+        font-family: 'Helvetica', Arial;
+        color: #464646;
+        text-transform: uppercase;
+        border-bottom: 1px solid #f1f1f1;
+        padding-bottom: 15px;
+        font-size: 28px;
+        margin-top: 0;
+    }
+
+    .Chart {
+        padding: 20px;
+        box-shadow: 0px 0px 20px 2px rgba(0, 0, 0, .4);
+        border-radius: 20px;
+        margin: 50px 0;
+    }
+</style>
