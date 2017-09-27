18c8ad4309c8a59069aa4dafc620ea2b920021cc
jameshmread <jamesread117@yahoo.co.uk>
2017-09-27 15:27:26 +0100

    csv upload and csv confirm are now styled

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,27 +9,23 @@
                 <div class="panel-heading">@yield('title')</div>
                 <div class="panel-body">
                     <div class="container">
-
-                        <div class="col-md-6">
+                        <div class="col-md-4">
                             <form action="{{route('admin.users.index')}}" method="GET">
-                                <button type="submit" class="btn btn-primary">Users Panel</button>
+                                <button type="submit" class="btn btn-primary btn-lg">Users Panel</button>
                             </form>
                         </div>
-
-                        <div class="col-md-6">
+                        <div class="col-md-4">
                             <form action="{{route('admin.upload.index')}}" method="GET">
-                                <button type="submit" class="btn btn-primary">Upload CSV File</button>
+                                <button type="submit" class="btn btn-primary btn-lg">Upload CSV File</button>
                             </form>
                         </div>
-
+                        <div class="col-md-4">
+                            <form action="{{route('admin.store.colour')}}" method="GET">
+                                <button type="submit" class="btn btn-primary btn-lg">Change Store Colours</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
-
-            <div class="col-md-3">
-                <form action="{{route('admin.store.colour')}}" method="GET">
-                    <button type="submit" class="btn btn-primary">Change Store Colours</button>
-                </form>
             </div>
         </div>
     </div>
