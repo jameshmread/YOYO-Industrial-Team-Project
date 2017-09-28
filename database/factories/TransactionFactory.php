83e46736e76f9b5bc4217222e7246d37b1d4aa8a
link5478 <cheyne.carsten@gmail.com>
2017-09-28 10:56:07 +0100

    Will only ever have positive transactions

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -6,9 +6,6 @@ $factory->define(App\Transaction::class, function (Faker $faker) {
 
     $cash = $faker->randomFloat(2, 0, 999);
     $discount = 0-$faker->randomFloat(2, 0, 999);
-    if($discount + $cash < 0) {
-        $discount = $cash;
-    }
     $customer = App\Customer::pluck('id')->All();
     $store = App\Store::pluck('outlet_reference')->All();
     $store2 = App\Store::pluck('outlet_name')->All();
@@ -21,7 +18,7 @@ $factory->define(App\Transaction::class, function (Faker $faker) {
         'transaction_type' => $faker->randomElement(['Payment', 'Refund', 'Redemption']),
         'cash_spent' => $cash,
         'discount_amount' => $discount,
-        'total_amount' => $cash + $discount,
+        'total_amount' => $cash,
         'transaction_hash' => $faker->md5
     ];
 });
